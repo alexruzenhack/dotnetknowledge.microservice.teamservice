c7fa6b98fdfcc2983b181c77577e01f9ed20defc
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2018-10-08 20:19:12 -0300

    Add integration tests

---
+++ TransferoTeam.Tests.Integration/SimpleIntegrationTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using TransferoTeam.Models;
+using Microsoft.AspNetCore.Hosting;
+using Xunit;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace TransferoTeam.Tests.Integration
+{
+    public class SimpleIntegrationTests
+    {
+        private readonly TestServer testServer;
+        private readonly HttpClient testClient;
+
+        private readonly Team teamZombie;
+
+        public SimpleIntegrationTests()
+        {
+            testServer = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            testClient = testServer.CreateClient();
+
+            teamZombie = new Team()
+            {
+                ID = Guid.NewGuid(),
+                Name = "Zombie"
+            };
+        }
+
+        [Fact]
+        public async void TestTeamPostAndGet()
+        {
+            StringContent stringContent = new StringContent(
+                JsonConvert.SerializeObject(teamZombie),
+                UnicodeEncoding.UTF8,
+                "application/json");
+
+            HttpResponseMessage postResponse = await testClient.PostAsync( "/api/teams", stringContent);
+            postResponse.EnsureSuccessStatusCode();
+
+            var getResponse = await testClient.GetAsync("/api/teams");
+            getResponse.EnsureSuccessStatusCode();
+
+            string raw = await getResponse.Content.ReadAsStringAsync();
+            List<Team> teams = JsonConvert.DeserializeObject<List<Team>>(raw);
+
+            Assert.Equal(1, teams.Count());
+            Assert.Equal("Zombie", teams[0].Name);
+            Assert.Equal(teamZombie.ID, teams[0].ID);
+        }
+    }
+}
